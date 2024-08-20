2c5bb48dad732a29110784175534f38de7f0c7f3
Cody Keener <codymkeener@gmail.com>
2024-08-19 21:26:43 -0500

    Added shotData.js with promises for shots

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,16 +11,20 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>Espresso Tracker</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} className="me-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
-            </Link>
-            <Button variant="danger" onClick={signOut}>Sign Out</Button>
+            <div>
+              <Link passHref href="/">
+                <Nav.Link>Home</Nav.Link>
+              </Link>
+            </div>
+            <div>
+              <Button variant="danger" onClick={signOut}>Sign Out</Button>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
